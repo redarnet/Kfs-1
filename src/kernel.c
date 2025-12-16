4273cda9f0c23eadc9dfc643df24df2b0cb14da4
Remi DARNET <redarnet@f4r7s7.clusters.42paris.fr>
2025-12-16 18:56:46 +0100

    keyboard handle

--- src/kernel.c
+++ src/kernel.c
@@ -32,6 +32,18 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
 
+static const char scancode_table[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6',
+    '7', '8', '9', '0', '-', '=', '\b',
+    '\t','q', 'w', 'e', 'r', 't', 'y',
+    'u', 'i', 'o', 'p', '[', ']', '\n',
+    0,  'a', 's', 'd', 'f', 'g', 'h',
+    'j', 'k', 'l', ';', '\'', '`', 0,
+    '\\','z', 'x', 'c', 'v', 'b', 'n',
+    'm', ',', '.', '/', 0, '*', 0, ' ',
+};
+
+
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
 {
 	return fg | bg << 4;
@@ -49,6 +61,19 @@ size_t strlen(const char* str)
 		len++;
 	return len;
 }
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+
 
 #define VGA_WIDTH   80
 #define VGA_HEIGHT  25
@@ -58,6 +83,28 @@ size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer = (uint16_t*)VGA_MEMORY;
+
+void cursor_enable(uint8_t start, uint8_t end)
+{
+    outb(0x3D4, 0x0A);
+    outb(0x3D5, (inb(0x3D5) & 0xC0) | start);
+
+    outb(0x3D4, 0x0B);
+    outb(0x3D5, (inb(0x3D5) & 0xE0) | end);
+}
+
+void cursor_update(size_t row, size_t col)
+{
+    uint16_t pos = row * VGA_WIDTH + col;
+
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (uint8_t)(pos & 0xFF));
+
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
+}
+
+
 
 // scroll a tester 
 void vga_scroll(void) {
@@ -69,6 +116,9 @@ void vga_scroll(void) {
 	uint16_t blank = vga_entry(' ', terminal_color);
     for (int col = 0; col < VGA_WIDTH; ++col)
         terminal_buffer[(VGA_HEIGHT-1) * VGA_WIDTH + col] = blank;
+	terminal_row = VGA_HEIGHT - 1;
+	cursor_update(terminal_row, terminal_column);
+
 }
 
 void terminal_initialize(void) 
@@ -76,6 +126,8 @@ void terminal_initialize(void)
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	cursor_enable(0, 15);
+	cursor_update(0, 0);
 	
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -101,7 +153,30 @@ void terminal_putchar(char c)
     if (c == '\n') {
         terminal_column = 0;
         terminal_row++;
-    } else {
+    }
+	else if (c == '\b') {
+		if (terminal_column > 0) {
+			terminal_column--;
+			terminal_putchar(' ');
+			terminal_column--;
+			cursor_update(terminal_row, terminal_column);
+		}
+		else if (terminal_row >  0)
+		{
+			terminal_row--;
+			terminal_column = VGA_WIDTH;
+			uint16_t blank = vga_entry(' ', terminal_color);
+			while (terminal_buffer[terminal_row * VGA_WIDTH + terminal_column] == blank)
+				terminal_column--;
+			// terminal_putchar(' ');
+			cursor_update(terminal_row, terminal_column);
+		}
+		return;
+	}
+	// 	uint16_t blank = vga_entry(' ', terminal_color);
+    // for (int col = 0; col < VGA_WIDTH; ++col)
+    //     terminal_buffer[(VGA_HEIGHT-1) * VGA_WIDTH + col] = blank;
+	else {
         terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
         terminal_column++;
     }
@@ -115,6 +190,8 @@ void terminal_putchar(char c)
         vga_scroll();
         terminal_row = VGA_HEIGHT - 1;
     }
+	cursor_update(terminal_row, terminal_column);
+
 }
 
 
@@ -131,6 +208,25 @@ void terminal_writestring(const char* data)
 }
 
 
+void keyboard_handle_input(void)
+{
+    static uint8_t last_scancode = 0;
+
+    uint8_t scancode = inb(0x60);
+
+    if (scancode == last_scancode)
+        return;
+
+    last_scancode = scancode;
+
+    if (scancode & 0x80)
+        return;
+
+    char c = scancode_table[scancode];
+    if (c)
+        terminal_putchar(c);
+}
+
 // cursor
 // https://wiki.osdev.org/Text_Mode_Cursor
 
@@ -139,10 +235,15 @@ void kernel_main(void)
 	/* Initialize terminal interface */
 	terminal_initialize();
 	/* Newline support is left as an exercise. */
-	terminal_writestring("42");
-	for (int i = 0; i < 30; i++)
-	{
-    	terminal_writestring("Hello 42\n");
-	}
+	// terminal_writestring("42");
+	// for (int i = 0; i < 30; i++)
+	// {
+    // 	terminal_writestring("Hello 42\n");
+	// }
+	// terminal_writestring("42");
+	while (1)
+        keyboard_handle_input();
+	// terminal_writestring("Hello\nWorld\n42\n");
+
 
 }
